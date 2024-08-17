1b09f979caae28930ca1453d466ab5baba565f9d
ecamposg <ecamposg95@gmail.com>
2024-08-17 14:41:05 -0600

    clear

--- Qr Generator/Scrip/QR_v2.py
+++ Qr Generator/Scrip/QR_v2.py
@@ -16,7 +16,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Datos que deseas incluir en el código QR
-data = "https://portalhcd.diputados.gob.mx/PortalWeb/Micrositios/723fc6d1-9b1d-44fe-a05e-ab6067047f75.pdf"
+data = "https://forms.gle/6at3toYjjTZ9goJH7"
 
 # Generar el código QR
 qr = qrcode.QRCode(
@@ -32,7 +32,7 @@ qr.make(fit=True)
 img = qr.make_image(image_factory=SvgImage)
 
 # Ruta del archivo donde se guardará el código QR en formato SVG
-file_path = os.path.join(output_dir, 'curso_sistema_alta_densidad.svg')
+file_path = os.path.join(output_dir, 'DINAMICA.svg')
 
 # Guardar la imagen en formato SVG
 with open(file_path, 'w', encoding='utf-8') as f:
